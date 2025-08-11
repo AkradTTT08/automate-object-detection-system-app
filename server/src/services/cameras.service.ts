939e5acfac33f9c362bee929597c67386a403c80
MERCURY <wanasart2@gmail.com>
2025-08-12 00:42:43 +0700

    test

--- server/src/services/cameras.service.ts
+++ server/src/services/cameras.service.ts
@@ -1,6 +1,7 @@
 import { pool } from '../config/db';
 
 export async function listCameras() {
+    console.log("Fetching cameras from the database...");
     const result = await pool.query(
         "SELECT * FROM cameras"
     );
