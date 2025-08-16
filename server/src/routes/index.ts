3d23d01407ba7ba58b029b1102d3337e6d0c0836
คนดีในดวงใจ😘 <kibsyaengk1368@gmail.com>
2025-08-16 16:45:49 +0700

    feat(api): เพิ่มการดึงรายการ maintenance ทั้งหมดมาแสดง

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,6 +3,7 @@ import cameras from './cameras.routes'
 import login from './login.routes';
 import register from './register.routes';
 import alerts from './alerts.routes';
+import maintenance from './maintenance.routes';
 
 const router = Router();
 
@@ -17,5 +18,8 @@ router.use('/cameras', cameras);
 
 // Alerts routes
 router.use('/alerts', alerts);
+
+// Maintenance
+router.use('/maintenanceHistory', maintenance);
 
 export default router;
