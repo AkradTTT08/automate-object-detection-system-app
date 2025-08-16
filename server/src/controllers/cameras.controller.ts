744fcd9e5754449901b3f4888c678fb268a8997c
Lincon001 <audomsak.kofak@gmail.com>
2025-08-16 18:58:36 +0700

    feat(cameras): อัปเดตสถานะ online/offline

--- server/src/controllers/cameras.controller.ts
+++ server/src/controllers/cameras.controller.ts
@@ -9,3 +9,19 @@ export async function list(req: Request, res: Response, next: NextFunction){
         next(err);
     }
 };
+
+export async function change(req: Request, res: Response, next: NextFunction){
+    try {
+        const { id, status } = req.body; // รับ id และ status ใหม่
+        console.log(id , status)
+
+        if (isNaN(id) || isNaN(status)) {
+            return res.status(400).json({ message: "id and status are required" });
+        }
+
+        const updatedCamera = await CameraService.changeStatus(id, status);
+        res.json(updatedCamera);
+    } catch(err) {
+        next(err);
+    }
+};
--- server/src/routes/cameras.routes.ts
+++ server/src/routes/cameras.routes.ts
@@ -4,5 +4,7 @@ import * as ctrl from '../controllers/cameras.controller'
 const router = Router();
 
 router.get('/', ctrl.list);
+router.patch('/', ctrl.change);
+
 
 export default router;
