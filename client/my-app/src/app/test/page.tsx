ee0b62ce6701cf218468fc72df0d77014380d771
MERCURY <wanasart2@gmail.com>
2025-08-21 03:18:41 +0700

    feat(modal): ทำ component register form

---
+++ client/my-app/src/app/test/page.tsx
@@ -0,0 +1,13 @@
+// Page ใช้สำหรับดึง component มาแสดงผลเพื่อทดสอบ
+'use client';
+
+import RegisterForm from "../components/RegisterForm";
+
+export default function Page() {
+
+  return (
+    <div className="flex flex-col gap-4">
+        <RegisterForm></RegisterForm>
+    </div>
+  );
+}
