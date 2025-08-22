ff1baa10c9a587ff96c29007eb7ad29e955f92ee
Jfyy-x <fasaiswy502@gmail.com>
2025-08-22 18:13:28 +0700

    feat(ui): form create alert

--- client/my-app/src/app/(with-layout)/test/page.tsx
+++ client/my-app/src/app/(with-layout)/test/page.tsx
@@ -1,9 +1,11 @@
+import FormCreateAlert from "../../components/FormCreateAlert"
+
 export default async function Page() {
 
   return (
     <div className="rounded-lg bg-[var(--color-white)] shadow-md p-6">
       <div className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-6">
-        
+        <FormCreateAlert></FormCreateAlert>
       </div>
     </div>
   );
