b5f6e953f2389b81bca6e34159d15c7e00cf5cdb
MERCURY <wanasart2@gmail.com>
2025-08-23 21:03:46 +0700

    feat(ui): เพิ่ม Badge

--- client/my-app/src/app/(with-layout)/test/page.tsx
+++ client/my-app/src/app/(with-layout)/test/page.tsx
@@ -3,16 +3,16 @@
 import FormCreateAlert from "../../components/FormCreateAlert"
 import IconPickerDemo from "../../components/IconPickerDemo";
 import CreateEventForm from "../../components/CreateEventForm";
-import FullScreenView from "@/app/components/FullScreenView";
+import Badge from "../../components/Badge";
 
 export default function Page() {
   return (
     <div className="rounded-lg bg-[var(--color-white)] shadow-md p-6">
       <div className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-6">
         <IconPickerDemo />
-        <FormCreateAlert></FormCreateAlert>
+        <FormCreateAlert />
         <CreateEventForm />
-        {/* <FullScreenView /> */}
+        {/* <Badge /> */}
       </div>
     </div>
   );
