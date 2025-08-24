26dbc29098317c9554dab7bd706105fa3bbfc91d
Lincon001 <audomsak.kofak@gmail.com>
2025-08-25 00:05:02 +0700

    feat(camera):Create note in maintenance

--- client/my-app/src/app/(with-layout)/test/page.tsx
+++ client/my-app/src/app/(with-layout)/test/page.tsx
@@ -4,15 +4,14 @@ import FormCreateAlert from "../../components/FormCreateAlert"
 import IconPickerDemo from "../../components/IconPickerDemo";
 import CreateEventForm from "../../components/CreateEventForm";
 import Badge from "../../components/badge";
-import TimeLineCard from "../../components/RelatedCard"
-import RelatedCard from "../../components/RelatedCard";
+import Createnote from "../../components/CreateNote"
 
 
 export default function Page() {
   return (
     <div className="rounded-lg bg-[var(--color-white)] shadow-md p-6">
       <div className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-6">
-        <RelatedCard/>
+        <Createnote/>
 
       </div>
     </div>
