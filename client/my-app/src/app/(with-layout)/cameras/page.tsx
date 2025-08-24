cfa41603eaa4a4f35f8638562bb32294ab53e201
66160348 <Moohchokchai.3000@gmail.com>
2025-08-24 15:14:24 +0700

    feat(camera): form เพิ่มข้อมูลกล้อง + validate

--- client/my-app/src/app/(with-layout)/cameras/page.tsx
+++ client/my-app/src/app/(with-layout)/cameras/page.tsx
@@ -1,7 +1,7 @@
 import CameraCard, { type Camera } from "../../components/cameraCard";
 import * as StatusCard from "../../components/StatusCard";
 import CamerasGrid from "../../components/searchCameras";
-
+import Form, { type createCamera } from "../../components/CreateCameraForm";
 
 const base = process.env.NEXT_PUBLIC_APP_URL!;
 
@@ -11,16 +11,20 @@ export default async function CamerasPage() {
   if (!res.ok) {
     throw new Error("Failed to load cameras");
   }
+  
+  const data = await res.json();
+  console.log("🔍 data from API:", data);
 
-  const cameras: Camera[] = await res.json();
+  const cameras: Camera[] = data;
+  const datas: createCamera[] = data;
 
   return (
     <div className="space-y-6">
       <StatusCard.DashboardSummaryCameraSection></StatusCard.DashboardSummaryCameraSection>
       <div className="rounded-lg bg-[var(--color-white)] shadow-md p-6 ">
         {/* <div className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-6"> */}
+        <Form datas={datas} />
         <CamerasGrid cameras={cameras} />
-        
       </div>
     </div>
   );
