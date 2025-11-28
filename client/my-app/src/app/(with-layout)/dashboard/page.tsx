7426c3c011b9d017ad719e33c9c0da79745eff46
Jfyy-x <fasaiswy502@gmail.com>
2025-11-29 02:02:11 +0700

    feat(table): Camera Alert Table

--- client/my-app/src/app/(with-layout)/dashboard/page.tsx
+++ client/my-app/src/app/(with-layout)/dashboard/page.tsx
@@ -1,7 +1,26 @@
 import EventSeverityChart from '@/app/components/Dashboard/SeverityDistributionChart';
 import AlertBarChart from '@/app/components/Dashboard/AlertAcorssCamerasChart';
+import CameraAlertView from '@/app/components/Dashboard/CameraAlertView';
 
-export default function Dashboard() {
+type SP = Record<string, string | string[] | undefined>;
+
+export default async function Dashboard({
+    searchParams,
+}: {
+    searchParams: Promise<SP>;
+}) {
+
+    const sp = await searchParams;
+
+    const pick = (key: keyof SP) =>
+        typeof sp?.[key] === "string" ? (sp[key] as string) : undefined;
+
+    const viewParam = pick("view");
+    const q = pick("q") ?? "";
+    const status = pick("status") as "Active" | "Inactive" | undefined;
+    const location = pick("location");
+    const type = pick("type");
+
     return (
         <div className="rounded-lg bg-[var(--color-white)] shadow-md p-6">
             <div className="rounded-lg bg-[var(--color-white)] shadow-md p-6">
@@ -9,6 +28,13 @@ export default function Dashboard() {
             </div>
             <div className="rounded-lg bg-[var(--color-white)] shadow-md p-6 mt-6">
                 <AlertBarChart />
+            </div>
+            <div className="rounded-lg bg-[var(--color-white)] shadow-md p-6 mt-6">
+                <CameraAlertView
+                    search={q}
+                    status={status}
+                    location={location}
+                    type={type} />
             </div>
         </div>
     );
