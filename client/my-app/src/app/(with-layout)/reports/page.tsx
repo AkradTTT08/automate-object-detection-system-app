0294ac050653ca7ec7639433ee5166c87b07cc38
Lincon001 <audomsak.kofak@gmail.com>
2025-11-28 17:07:00 +0700

    feat(Report):add menu template

--- client/my-app/src/app/(with-layout)/reports/page.tsx
+++ client/my-app/src/app/(with-layout)/reports/page.tsx
@@ -1,9 +1,9 @@
+import ReportMenu from "@/app/components/Reports/ReportMenu";
+
 export default function Reports() {
     return (
         <div className="rounded-lg bg-[var(--color-white)] shadow-md p-6">
-            <div className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-6">
-                <h1 className="text-2xl font-bold">Reports</h1>
-            </div>
+            <ReportMenu />
         </div>
     );
 }
